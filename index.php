f46deeb8817fbffea79f0843ccd30c62d1ba8cbc
jaimefrancosalazar <jaimefrancosalazar@gmail.com>
2020-06-11 11:54:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p> page using OpenShift Online test 2, now jenkins</p>'; ?> 
+ <?php echo '<p> page using OpenShift Online test 2 now jenkins 2</p>'; ?> 
  </body>
 </html>
